f994e30ba051ab3f86b87e8a12eb54e9ec1d9017
wilgawox <Thibault Pradal>
2021-08-13 12:08:22 +0200

    End of stage commit n.2

--- src/openalea/oalab/service/qt_control.py
+++ src/openalea/oalab/service/qt_control.py
@@ -143,7 +143,12 @@ def qt_painter(control, shape=None, preferred=None):
 
 def edit(control):
     import sys
-    if 'PyQt5.QtGui' in sys.modules or 'PySide.QtGui' in sys.modules:
+    if (
+        'PyQt5.QtGui' in sys.modules or
+        'PySide.QtGui' in sys.modules or 
+        'Pyside2.QtGui' in sys.modules or 
+        'PyQt4.QtGui' in sys.modules 
+        ) :
         from openalea.vpltk.qt import QtGui
         if QtGui.QApplication.instance():
             if isinstance(control, Control):
