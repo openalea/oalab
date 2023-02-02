36379fe2a1af4a4b64bcc0cca28ef4f429da43b0
pradal <christophe.pradal@inria.fr>
2023-02-02 21:32:32 +0100

    Update version and add CI

---
+++ src/openalea/oalab/version.py
@@ -0,0 +1,16 @@
+"""
+Version for this package.
+"""
+#  -*- coding: utf-8 -*-
+
+MAJOR = 2
+"""(int) Version major component."""
+
+MINOR = 3
+"""(int) Version minor component."""
+
+POST = 0
+"""(int) Version post or bugfix component."""
+
+__version__ = ".".join([str(s) for s in (MAJOR, MINOR, POST)])
+
